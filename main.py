7a6f5a03e99c6d01dba0ed690703556804662c69
eduardo <eduardo.campos@meuprovedor.com.br>
2026-06-09 00:23:51 -0300

    terminei a primeira videoaula

--- main.py
+++ main.py
@@ -17,8 +17,33 @@ background = pygame.transform.scale(pygame.image.load("C:/Users/nuvem/Desktop/in
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
-        self.image = pygame.transform.rotozoom(pygame.image.load('C:/Users/nuvem/Desktop/inf1034-atividade13-JOGO/Hero_Walk_01.png').convert_alpha(), 0, 0.62)
+        self.image = pygame.transform.rotozoom(pygame.image.load('C:/Users/nuvem/Desktop/inf1034-atividade13-JOGO/Hero_Walk_01.png').convert_alpha(), 0, PLAYER_SIZE)
         self.pos = pygame.math.Vector2(PLAYER_START_X, PLAYER_START_Y)
+        self.speed = PLAYER_SPEED
+    def user_input(self):
+        self.velocity_x = 0
+        self.velocity_y = 0
+
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_w]:
+            self.velocity_y = -self.speed
+        if keys[pygame.K_d]:
+            self.velocity_x = self.speed
+        if keys[pygame.K_s]:
+            self.velocity_y = self.speed
+        if keys[pygame.K_a]:
+            self.velocity_x = -self.speed
+
+        if self.velocity_x != 0 and self.velocity_y != 0: # jogador esta se mexendo na diagonal
+            self.velocity_x /= math.sqrt(2)
+            self.velocity_y /= math.sqrt(2)
+    def move(self):
+        self.pos += pygame.math.Vector2(self.velocity_x, self.velocity_y)
+    def update(self):
+        self.user_input()
+        self.move()
+
 
 player = Player()
 
@@ -31,5 +56,6 @@ while True:
 
     screen.blit(background, (0,0))
     screen.blit(player.image, player.pos)
+    player.update()
     pygame.display.update()
     clock.tick(FPS)
